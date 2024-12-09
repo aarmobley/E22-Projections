e24af3f0da751c04e5ff05eb643d42305ae090e3
Aaron Mobley <aarmobley@gmail.com>
2024-12-09 11:56:45 -0500

    Update St. Johns.py

--- pages/St. Johns.py
+++ pages/St. Johns.py
@@ -177,7 +177,7 @@ if select_event == 'Easter':
         color = "red" if kids_capacity > 80 else "blue"
         st.markdown(f"<p style='color:{color}; font-size:18px;'>Capacity: {kids_easter_capacity:.0f}%</p>", unsafe_allow_html=True)
     
-    else: 
+else: 
         st.write(f"Projected Kids Attendance: {kids_1122: .0f}")
          ## HTML and markdown for kids capacity
         color = "red" if kids_capacity > 80 else "blue"
