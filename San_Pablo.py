3082864191662e1c40c77860122b122cd4b8fa7c
Aaron Mobley <aarmobley@gmail.com>
2025-03-12 09:15:59 -0400

    Update San_Pablo.py

--- San_Pablo.py
+++ San_Pablo.py
@@ -241,7 +241,7 @@ if st.button("Make Projection"):
             st.write(f"Projected Adult Attendance: {inclement_weather:.0f}")
             
             color = "red" if capacity > 80 else "blue"
-            st.markdown(f"<p style='color:{color}; font-size:18px;'>Capacity: {capacity:.0f}%</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='color:{color}; font-size:18px;'>Worship Center Capacity: {capacity:.0f}%</p>", unsafe_allow_html=True)
     
     ### HTML and MArkdown for adult capacity
         #color = "red" if capacity > 80 else "blue"
