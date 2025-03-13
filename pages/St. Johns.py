9e1b6d7feff0551bc2a64e47b0e6e9704eed9671
Aaron Mobley <aarmobley@gmail.com>
2025-03-13 10:09:40 -0400

    Update St. Johns.py

--- pages/St. Johns.py
+++ pages/St. Johns.py
@@ -164,9 +164,9 @@ if st.button("Make Projection"):
     
     st.write(f"Projected Adult Attendance: {prediction:.0f}")
     
-    color = "red" if capacity > 80 else "blue"
+    color = "red" if capacity > 70 else "blue"
     
-    st.markdown(f"<p style='color:{color}; font-size:18px;'>Capacity: {capacity:.0f}%</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='color:{color}; font-size:18px;'>Worship Center Capacity: {capacity:.0f}%</p>", unsafe_allow_html=True)
     
     st.divider()
     
