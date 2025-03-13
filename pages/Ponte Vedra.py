1dd828e37605e2414133683fb6f355c29301283b
Aaron Mobley <aarmobley@gmail.com>
2025-03-13 10:09:17 -0400

    Update Ponte Vedra.py

--- pages/Ponte Vedra.py
+++ pages/Ponte Vedra.py
@@ -171,9 +171,9 @@ if st.button("Make Projection"):
     
     
     ### HTML and MArkdown for adult capacity
-    color = "red" if capacity > 80 else "blue"
+    color = "red" if capacity > 70 else "blue"
     
-    st.markdown(f"<p style='color:{color}; font-size:18px;'>Capacity: {capacity:.0f}%</p>", unsafe_allow_html=True)                                       #st.write(f"Adult Capacity: {capacity: .0f}%")
+    st.markdown(f"<p style='color:{color}; font-size:18px;'>Worship Center: {capacity:.0f}%</p>", unsafe_allow_html=True)                                       #st.write(f"Adult Capacity: {capacity: .0f}%")
     
     
   
