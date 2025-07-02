8951aa9dea946ce0bacbbd259342122a32670648
Aaron Mobley <aarmobley@gmail.com>
2025-07-02 16:46:34 -0400

    Update St. Johns.py

--- pages/St. Johns.py
+++ pages/St. Johns.py
@@ -186,11 +186,11 @@ if st.button("Make Projection"):
         st.markdown(f"<p style='color:{color}; font-size:18px;'>Capacity: {kids_capacity:.0f}%</p>", unsafe_allow_html=True)
 
 
-   st.divider
+st.divider
 
 
 
-    title = st.title("St. Johns Students Attendance Projection")
+title = st.title("St. Johns Students Attendance Projection")
 
 
 
