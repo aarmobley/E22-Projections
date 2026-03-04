1596503d2c4596c6385db60421bc838284b495d6
Aaron Mobley <aarmobley@gmail.com>
2026-03-04 14:39:50 -0500

    Update All Campus Projections.py

--- All Campus Projections.py
+++ All Campus Projections.py
@@ -8,14 +8,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed")
 
-# Hide sidebar completely
-st.markdown("""
-<style>
-    [data-testid="stSidebar"] {display: none;}
-    [data-testid="stSidebarCollapsedControl"] {display: none;}
-</style>
-""", unsafe_allow_html=True)
-
 
 st.markdown("""
 <div style="text-align: center; margin-bottom: 20px;">
